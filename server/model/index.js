b3c832003b9c526f3206815d7f7334b9fc26696e
William Tucker <william.tucker29@gmail.com>
2024-04-06 15:27:24 -0500

    Created GET request path for admin information.

--- server/model/index.js
+++ server/model/index.js
@@ -1,9 +1,11 @@
 const getClasses = require('./getClasses');
 const getTeachers = require('./getTeachers');
 const getStudents = require('./getStudents');
+const getAdmin = require('./getAdmin');
 
 module.exports = {
   getClasses,
   getTeachers,
   getStudents,
+  getAdmin,
 };
--- server/router.js
+++ server/router.js
@@ -3,7 +3,7 @@ const controller = require('./controller');
 
 // GET requests
 // router.get('/roles', controller.getRoles);
-// router.get('/admin', controller.getAdmin);
+router.get('/admin', controller.getAdmin);
 router.get('/classes', controller.getClasses);
 router.get('/teachers', controller.getTeachers);
 // router.get('/teachers/assignments/:teacherId', controller.getTeacherAssignments);
