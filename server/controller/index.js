024367267a2ae670c6a74bebb72b53a41820bd80
William Tucker <william.tucker29@gmail.com>
2024-04-06 15:23:54 -0500

    Added controllers for getStudents and getTeachers.

--- server/controller/index.js
+++ server/controller/index.js
@@ -1,5 +1,9 @@
 const getClasses = require('./getClasses');
+const getTeachers = require('./getTeachers');
+const getStudents = require('./getStudents');
 
 module.exports = {
   getClasses,
+  getTeachers,
+  getStudents,
 };
