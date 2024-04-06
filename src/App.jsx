33cfc1682122aef63d134ccd9121adfd27a1f740
Richard Li <richardli1745@gmail.com>
2024-04-06 13:49:25 -0700

    added css changes to Login Page and Moved login page into components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Login from './components/Login';
 import './styles.css';
 
 function App() {
   return (
-    <div>
-      <h1>SkoolHub</h1>
+    <div className="login-page">
+      <h1 className="login-h1">SkoolHub</h1>
+      <Login />
     </div>
   );
 }
