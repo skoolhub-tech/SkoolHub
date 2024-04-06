7900a0bb1f35a09f945749bcdf84b70e6211a31a
Michael O'Brien <mob61887@gmail.com>
2024-04-05 20:37:52 -0400

    merging with main

---
+++ src/components/SubmitAssignmentButton.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+function SubmitAssignmentButton() {
+  return (
+    <div>
+      <button type="button" className="submitAssignmentButtonButton">Submit Assignment</button>
+    </div>
+  );
+}
+
+export default SubmitAssignmentButton;
