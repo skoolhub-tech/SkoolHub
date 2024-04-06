9f0646478d24698631b67fa22c7fc17f9c4685c0
jreagant <jr.tankersley@gmail.com>
2024-04-06 18:21:39 -0400

    moved changes from main

--- src/components/data-providers/UserDataProvider.jsx
+++ src/components/data-providers/UserDataProvider.jsx
@@ -3,11 +3,16 @@ import PropTypes from 'prop-types';
 import axios from 'axios';
 
 const Context = React.createContext();
-const { useState, useEffect, useContext } = React;
+const {
+  useState,
+  useEffect,
+  useContext,
+  useMemo,
+} = React;
 
 // Data provider component
-export function DataProvider({ children }) {
-  const [data, setData] = useState({});
+export function UserDataProvider({ children }) {
+  const [userData, setUserData] = useState({});
 
   // Un-comment to perform some get request on mount
   // useEffect(() => {
@@ -16,20 +21,22 @@ export function DataProvider({ children }) {
   //     .catch((error) => console.error(error));
   // }, []);
 
+  const userState = useMemo(() => ({ userData, setUserData }), []);
+
   // Returns wrapper component for all components
   // Pass state and methods to value prop for access in other components
   return (
-    <Context.Provider value={data /* additional state, methods */}>
+    <Context.Provider value={userState}>
       {children}
     </Context.Provider>
   );
 }
 
-DataProvider.propTypes = {
+UserDataProvider.propTypes = {
   children: PropTypes.node.isRequired,
 };
 
-export const useData = () => useContext(Context);
+export const useUserData = () => useContext(Context);
 
 // To access state and methods inside this component,
 // import { useData } from './path/to/DataProvider'
