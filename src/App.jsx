e963be6cc6a8bcec2917ce55daa67f2753ae4551
jreagant <jr.tankersley@gmail.com>
2024-04-07 13:44:27 -0400

    removed prop from UserDataProvider

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
 
   return (
     <div className="navbar-container">
-      <UserDataProvider isLoggedIn={isLoggedIn}>
+      <UserDataProvider>
         <Router>
           {isLoggedIn ? (
             <>
