755f401d3c15d5da4e237d681fd3ae9213825156
jreagant <jr.tankersley@gmail.com>
2024-04-07 15:19:12 -0400

    stubbed homepage

---
+++ src/components/Homepage/Homepage.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import axios from 'axios';
+import { useUserData } from '../data-providers/UserDataProvider';
+
+const { useState, useEffect } = React;
+
+function Homepage() {
+  const { userData } = useUserData();
+  const [classes, setClasses] = useState([]);
+  const [currentClass, setCurrentClass] = useState('');
+  const [assignments, setAssignments] = useState([]);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    axios.get(`/skoolhub/classes/${userData.email}`)
+      .then((response) => setClasses(response.data))
+      .catch((error) => console.error({
+        Message: 'Error retrieving classes.',
+        Error: error,
+      }));
+    // get assignments
+    // get tasks
+  }, []);
+
+  return (
+    <div>
+      {/* <h1>Homepage</h1> */}
+      <select value={currentClass} onChange={(e) => setCurrentClass(e.target.Value)}>
+        <option value="">Select a class</option>
+        {classes.map((classObj) => (
+          <option key={classObj.id} value={classObj.id}>{classObj.name}</option>
+        ))}
+      </select>
+      {assignments.length > 0 && (
+        <div>
+          <h2>Assignments</h2>
+          <ul>
+            {assignments.map((assignment) => (
+              <li key={assignment.id}>{assignment.name}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+      {tasks.length > 0 && (
+        <div>
+          <h2>Today's Tasks</h2>
+          <ul>
+            {tasks.map((task) => (
+              <li key={task.id}>{task.name}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Homepage;
