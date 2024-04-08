0526c4d43f933b4ea881af10770ae51221a97521
jreagant <jr.tankersley@gmail.com>
2024-04-08 14:31:27 -0400

    corrected request for tasks based on role, id

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -6,31 +6,33 @@ const { useState, useEffect } = React;
 
 function Homepage() {
   const { userData } = useUserData();
+  const { email, id, role } = userData;
+
   const [classes, setClasses] = useState([]);
   const [currentClass, setCurrentClass] = useState('');
   const [assignments, setAssignments] = useState([]);
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
-    axios.get(`/skoolhub/classes/${userData.email}`)
+    axios.get(`/skoolhub/classes/${email}`)
       .then((response) => setClasses(response.data))
       .catch((error) => console.error({
         Message: 'Error retrieving classes.',
         Error: error,
       }));
     // get calendar
-    // axios.get(`/skoolhub/calendar/${userData.email}`)
-    //   .then((response) => setAssignments(response.data))
-    //   .catch((error) => console.error({
-    //     Message: 'Error retrieving assignments.',
-    //     Error: error,
-    //   }));
+    axios.get(`/skoolhub/calendar/${role}/${id}`)
+      .then((response) => setTasks(response.data))
+      .catch((error) => console.error({
+        Message: 'Error retrieving assignments.',
+        Error: error,
+      }));
     // get assignments
   }, []);
 
   return (
     <div>
-      {/* <h1>Homepage</h1> */}
+      <h1>Homepage</h1>
       <select value={currentClass} onChange={(e) => setCurrentClass(e.target.Value)}>
         <option value="">Select a class</option>
         {classes.map((classObj) => (
@@ -49,7 +51,7 @@ function Homepage() {
       )}
       {tasks.length > 0 && (
         <div>
-          <h2>Today's Tasks</h2>
+          <h2>Today&apos;s Tasks</h2>
           <ul>
             {tasks.map((task) => (
               <li key={task.id}>{task.name}</li>
