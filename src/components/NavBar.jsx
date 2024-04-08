9e006c2fc61a3ac78f251194218223a218cdb771
Richard Li <richardli1745@gmail.com>
2024-04-08 10:37:41 -0700

    changes to calendar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,6 +4,7 @@ import { useNavigate, useLocation } from 'react-router-dom';
 function NavBar() {
   const navigate = useNavigate();
   const location = useLocation();
+  console.log(location.pathname);
   const handleNavigation = (path) => {
     navigate(path);
   };
@@ -13,42 +14,42 @@ function NavBar() {
       <button
         type="button"
         onClick={() => handleNavigation('homepage')}
-        className={`${location.pathname === 'homepage' ? 'tab-active' : 'tab'}`}
+        className={`${location.pathname === '/homepage' ? 'tab-active' : 'tab'}`}
       >
         Home
       </button>
       <button
         type="button"
         onClick={() => handleNavigation('assignments')}
-        className={`${location.pathname === 'assignments' ? 'tab-active' : 'tab'}`}
+        className={`${location.pathname === '/assignments' ? 'tab-active' : 'tab'}`}
       >
         Assignments
       </button>
       <button
         type="button"
         onClick={() => handleNavigation('events')}
-        className={`${location.pathname === 'events' ? 'tab-active' : 'tab'}`}
+        className={`${location.pathname === '/events' ? 'tab-active' : 'tab'}`}
       >
         Events
       </button>
       <button
         type="button"
         onClick={() => handleNavigation('classes')}
-        className={`${location.pathname === 'classes' ? 'tab-active' : 'tab'}`}
+        className={`${location.pathname === '/classes' ? 'tab-active' : 'tab'}`}
       >
         Classes
       </button>
       <button
         type="button"
         onClick={() => handleNavigation('emails')}
-        className={`${location.pathname === 'emails' ? 'tab-active' : 'tab'}`}
+        className={`${location.pathname === '/emails' ? 'tab-active' : 'tab'}`}
       >
         Emails
       </button>
       <button
         type="button"
         onClick={() => handleNavigation('admin')}
-        className={`${location.pathname === 'admin' ? 'tab-active' : 'tab'}`}
+        className={`${location.pathname === '/admin' ? 'tab-active' : 'tab'}`}
       >
         Admin
       </button>
