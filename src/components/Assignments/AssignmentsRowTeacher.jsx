ecc0a95c364a4072edfa93d4233e563030e971ec
Michael O'Brien <mob61887@gmail.com>
2024-04-10 15:22:47 -0400

    moved edit assignt modal outside of table

--- src/components/Assignments/AssignmentsRowTeacher.jsx
+++ src/components/Assignments/AssignmentsRowTeacher.jsx
@@ -4,48 +4,45 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { FaEdit } from 'react-icons/fa';
 import { FaMagnifyingGlass } from 'react-icons/fa6';
-import EditAssignmentModal from './EditAssignmentModal';
 import formatDate from '../../utils/formatDate_Month_D_Y';
 
 function AssignmentsRowTeacher({
   assignment,
   setViewAssignmentSubmissions,
+  setEditSubmissionModalIsOpen,
+  setAssignmentToEdit,
+  setAssignmentId,
 }) {
-  const [editSubmissionModalIsOpen, setEditSubmissionModalIsOpen] = useState(false);
-
   function handleViewSubmissionsClick() {
     setViewAssignmentSubmissions(assignment);
+    setAssignmentId(assignment.id);
+  }
+
+  function handleEditAssignmentClick() {
+    setAssignmentToEdit(assignment);
+    setEditSubmissionModalIsOpen(true);
   }
 
   return (
-    <>
-      <tr>
-        <td>{assignment.name}</td>
-        <td>{formatDate(assignment.due_date)}</td>
-        <td className="edit_assignment_button">
-          <button type="button" onClick={() => setEditSubmissionModalIsOpen(true)}><FaEdit /></button>
-        </td>
-        <td className="view_submissions_button">
-          <button type="button" onClick={handleViewSubmissionsClick}><FaMagnifyingGlass size={15} /></button>
-        </td>
-      </tr>
-      {editSubmissionModalIsOpen && (
-      <EditAssignmentModal
-        assignment={assignment}
-        setEditSubmissionModalIsOpen={setEditSubmissionModalIsOpen}
-      />
-      )}
-    </>
+    <tr>
+      <td>{assignment.name}</td>
+      <td>{formatDate(assignment.due_date)}</td>
+      <td className="edit_assignment_button">
+        <button type="button" onClick={handleEditAssignmentClick}><FaEdit /></button>
+      </td>
+      <td className="view_submissions_button">
+        <button type="button" onClick={handleViewSubmissionsClick}><FaMagnifyingGlass size={15} /></button>
+      </td>
+    </tr>
   );
 }
 
 AssignmentsRowTeacher.propTypes = {
-  assignment: PropTypes.shape({
-    id: PropTypes.number,
-    name: PropTypes.string,
-    due_date: PropTypes.string,
-  }).isRequired,
+  assignment: PropTypes.object.isRequired,
   setViewAssignmentSubmissions: PropTypes.func.isRequired,
+  setEditSubmissionModalIsOpen: PropTypes.func.isRequired,
+  setAssignmentToEdit: PropTypes.func.isRequired,
+  setAssignmentId: PropTypes.func.isRequired,
 };
 
 export default AssignmentsRowTeacher;
