a7e6379fc038cda83784a267314ecdad2c97ddf4
Malcolm Kam <malcolm.h.kam@gmail.com>
2024-04-11 10:05:36 -1000

    fixed modal to work with animation

--- src/components/Task_list/Task.jsx
+++ src/components/Task_list/Task.jsx
@@ -6,16 +6,18 @@ import './task.css';
 function Task() {
   return (
     <motion.div
-      className="task-container"
+      className="o"
       initial={{ x: '100%' }}
       animate={{ x: '0%' }}
       transition={{ ease: 'easeInOut', duration: 0.7 }}
     >
+      <div className="task-container">
       <h1 className="tasks-header">
         Events & Tasks
       </h1>
       <div className="calendar-container">
         <TaskCalendar defaultView="month" views={['month', 'week', 'day', 'agenda']} />
+      </div>
       </div>
     </motion.div>
   );
