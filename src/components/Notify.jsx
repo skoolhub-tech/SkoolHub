2ca2fb71a7ad99a9392cac7073a2cab5f4c42865
mmcclanahan <mmmcclan365@gmail.com>
2024-04-11 15:29:34 -0700

    added notify component

---
+++ src/components/Notify.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import PropTypes from 'prop-types';
+import '../styles.css';
+/*
+example usage:
+const [notify, setNotify] = useState(false);
+const [message, setMessage] = useState('');
+const [color, setColor] = useState(0);
+
+function showNotificationTimer() {
+    setNotify(true);
+    setTimeout(() => {
+      setNotify(false);
+    }, 2000);
+  }
+
+  for the action thats returning a success or failure
+    set message with the message you want to display
+    set color to 0 for success, 1 for failure
+    showNotificationTimer();
+
+{notify && (
+  <Notify
+    message={message}
+    color={color}
+    icon={<MdOutlineMarkEmailRead />}
+  />
+)}
+*/
+
+function Notify({ message, color, icon }) {
+  return (
+    <motion.div
+      className={color === 0 ? 'notifyModalGreen' : 'notifyModalRed'}
+      initial={{ opacity: 0, scale: 0.1 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.2 }}
+      exit={{ scale: 0.5 }}
+    >
+      <div className="notifyModalContent">
+        {icon && icon}
+        <p>{message}</p>
+      </div>
+    </motion.div>
+  );
+}
+
+Notify.propTypes = {
+  message: PropTypes.string.isRequired,
+  color: PropTypes.number.isRequired,
+  icon: PropTypes.node,
+};
+
+Notify.defaultProps = {
+  icon: null,
+};
+
+export default Notify;
