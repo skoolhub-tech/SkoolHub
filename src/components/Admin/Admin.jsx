31dc6bed614af1352a641abf82abccc7b1e8643f
vlieu425 <121201991+vlieu425@users.noreply.github.com>
2024-04-11 12:02:20 -0700

    refactor code, fix linter errors, add prop types

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,12 +1,10 @@
 import React, { useState } from 'react';
 import AssignStudentClass from './AssignStudentClass';
-import { useUserData } from '../data-providers/UserDataProvider';
 import CreateUser from './CreateUser';
 import CreateClass from './CreateClass';
 import './admin.css';
 
 function Admin() {
-  const { userData } = useUserData();
   const [showUsersModal, setShowUsersModal] = useState(false);
   const [showClassesModal, setShowClassesModal] = useState(false);
 
@@ -25,21 +23,13 @@ function Admin() {
 
   return (
     <div className="admin-container">
-      {/* Your code here */}
       <div className="admin-create-buttons">
         <button type="button" onClick={handleUsersButtonClick}>Users</button>
         <button type="button" onClick={handleClassesButtonClick}>Classes</button>
       </div>
       <AssignStudentClass />
-      {/* Users Modal */}
-      {showUsersModal && (
-        <CreateUser exitModal={exitModal} />
-      )}
-
-      {/* Classes Modal */}
-      {showClassesModal && (
-        <CreateClass exitModal={exitModal} />
-      )}
+      {showUsersModal && <CreateUser exitModal={exitModal} />}
+      {showClassesModal && <CreateClass exitModal={exitModal} />}
     </div>
   );
 }
