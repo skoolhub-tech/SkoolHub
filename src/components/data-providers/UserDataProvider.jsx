f121c5407cecc0b9c45d2353186ad8bad46a2296
jreagant <jr.tankersley@gmail.com>
2024-04-12 11:21:10 -0400

    removed default userData

--- src/components/data-providers/UserDataProvider.jsx
+++ src/components/data-providers/UserDataProvider.jsx
@@ -13,10 +13,10 @@ const {
 // Data provider component
 export function UserDataProvider({ children }) {
   const [userData, setUserData] = useState({
-    role: 3,
-    name: 'John Smith',
-    email: 'john.smith@gmail.com',
-    id: 1,
+    role: null,
+    name: null,
+    email: null,
+    id: null,
   });
 
   useEffect(() => {
